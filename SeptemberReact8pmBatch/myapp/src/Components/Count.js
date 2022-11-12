415b0a51f74178272b5ae152bbab81c7b4607404
Arpitha-P_cdk <arpitha.p@cdk.com>
2022-11-12 20:54:39 +0530

    react state

--- SeptemberReact8pmBatch/myapp/src/Components/Count.js
+++ SeptemberReact8pmBatch/myapp/src/Components/Count.js
@@ -1,14 +1,31 @@
 import React, {Component} from "react";
 
 class Count extends Component {
+  constructor() {
+    super();
+    console.log(this, "constructor");
+    this.state = {
+      count: 0,
+      name: "sachin"
+    };
+  }
   incCount = () => {
-    alert(123);
+    console.log(this, "current object incCount");
+    this.setState({
+      count: this.state.count + 1,
+      name: "DHONI"
+    });
   };
+
   render() {
+    console.log("render got called", this);
     return (
       <div>
         <button onClick={this.incCount}>Inc Count</button>
-        <h1>0</h1>
+
+        <h1>
+          {this.state.count}....{this.state.name}
+        </h1>
       </div>
     );
   }
