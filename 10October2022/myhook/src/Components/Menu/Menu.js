bdad57d2e42b2ec0399eb9ac0e289067f085f5f9
Arpitha-P_cdk <arpitha.p@cdk.com>
2022-12-19 10:02:52 +0530

    onChange

--- 10October2022/myhook/src/Components/Menu/Menu.js
+++ 10October2022/myhook/src/Components/Menu/Menu.js
@@ -1,16 +1,22 @@
 import React from "react";
 import {Link, BrowserRouter, Routes, Route} from "react-router-dom";
+import {AboutUs} from "../AboutUs";
+import {GettingDataOnChange} from "../GettingDataOnChange";
 import {Home} from "../Home";
 
 export const Menu = () => {
   return (
-    <div>
+    <div id="menu">
       <BrowserRouter>
-        <Link to="/home">Home</Link>
-        <Link to="/about">AboutUs</Link>
+        <div id="menu-items">
+          <Link to="/home">Home</Link>
+          <Link to="/aboutUs">AboutUs</Link>
+          <Link to="/onChange">GettingData</Link>
+        </div>
         <Routes>
           <Route path="/home" element={<Home />} />
-          <Route path="/about" element={"arpitha"} />
+          <Route path="/aboutUs" element={<AboutUs />} />
+          <Route path="/onChange" element={<GettingDataOnChange />} />
         </Routes>
       </BrowserRouter>
     </div>
