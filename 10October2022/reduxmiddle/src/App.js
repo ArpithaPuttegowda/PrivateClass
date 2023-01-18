60c4f21286f9b65b0f7c35ffe3b69b6fecda9827
Arpitha-P_cdk <arpitha.p@cdk.com>
2023-01-18 09:47:45 +0530

    redux-toolkit

--- 10October2022/reduxmiddle/src/App.js
+++ 10October2022/reduxmiddle/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import ReduxMiddleware from "./Components/ReduxMiddleware";
+import {ReduxToolkitEx} from "./ReduxToolkit/ReduxToolkitEx";
 
 function App() {
   return (
     <div className="App">
       <ReduxMiddleware />
+      <h1>
+        <ReduxToolkitEx />
+      </h1>
     </div>
   );
 }
--- 10October2022/reduxmiddle/src/Store/store.js
+++ 10October2022/reduxmiddle/src/Store/store.js
@@ -4,11 +4,13 @@ import logger from "redux-logger";
 import thunk from "redux-thunk";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "../Actions/SagaAction/rootSaga";
+import nameLocRed from "../ReduxToolkit/slice";
 
 const saga = createSagaMiddleware();
 
 const rootReducer = combineReducers({
-  ajax: ajaxReducer
+  ajax: ajaxReducer,
+  nameLoc: nameLocRed
 });
 
 const store = configureStore({
