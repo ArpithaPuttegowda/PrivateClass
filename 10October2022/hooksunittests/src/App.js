b9f756d57c38f48ab69b9a168a426c8d3c82a1d1
Arpitha-P_cdk <arpitha.p@cdk.com>
2023-01-26 09:51:05 +0530

    useTransition

--- 10October2022/hooksunittests/src/App.js
+++ 10October2022/hooksunittests/src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import {Count} from "./Components/ExampleUseReducer/Count";
 import {CountReducer} from "./Components/ExampleUseReducer/CountReducer";
 import {GlobalStateManagement} from "./Components/ExampleUseReducer/GlobalStatementUseReducer/GlobalStateManagement";
+import {ExampleUseTransition} from "./Components/Hooks/ExampleUseTransition";
 
 function App() {
   return (
     <div className="App">
       {/* <Count />
       <CountReducer /> */}
-      <GlobalStateManagement />
+      {/* <GlobalStateManagement /> */}
+      <ExampleUseTransition />
     </div>
   );
 }
--- Novemer21/myreactapp/src/Components/Menu/Menu.js
+++ Novemer21/myreactapp/src/Components/Menu/Menu.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React, {useState, useTransition} from "react";
 
 export const Menu = () => {
-  return <div>Menu</div>;
+  const [isPending, startTransition] = useTransition();
+  const [listNumbers, setListNumbers] = useState([]);
+  const [input, setInput] = useState("");
+  const handleChange = (e) => {
+    setInput(e.target.value);
+    startTransition(() => {
+      const arr = [];
+      for (let i = 0; i < 5000; i++) {
+        arr.push(e.target.value);
+      }
+      setListNumbers(arr);
+    });
+  };
+
+  return (
+    <>
+      <input type="number" value={input} onChange={handleChange} />
+      {isPending
+        ? "loading..."
+        : listNumbers.map((item, i) => <div key={i}>{item}</div>)}
+    </>
+  );
 };
