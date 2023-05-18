584e06a0a9485a70b2be0128ed67a99b9a158119
ArpithaPuttegowda <arpithalekhya111@gmail.com>
2023-05-18 08:55:02 +0530

    getReference

---
+++ StepUp1May/GetReference.js
@@ -0,0 +1,16 @@
+
+
+// document.getElementsByTagName("div")[0].style.color="green"
+// document.getElementById("player").style.color="blue"
+
+// document.getElementsByClassName("player-name")[0].style.fontSize="100px"
+
+// document.getElementsByName("tech-stack")[0].style.color="pink"
+
+
+// querySelector and querySelectorAll
+document.querySelector("#player")
+document.querySelector(".player-name")
+document.querySelector(`[name='tech-stack']`)
+document.querySelectorAll("[name='tech-stack']")[0]
+document.querySelectorAll("[name='tech-stack']")[1]
