4a07a02295f1c6a0b760f8227e7c00d21a047607
ArpithaPuttegowda <arpithalekhya111@gmail.com>
2023-05-31 10:40:06 +0530

    unit tests

--- Jan30StepUp/myreactapphooks/src/Counter/Counter.js
+++ Jan30StepUp/myreactapphooks/src/Counter/Counter.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [inputValue, setInputValue] = useState("");
+  const handleChange = (eve) => {
+    setInputValue(eve?.target?.value);
+  };
+  const handleInc = () => {
+    setCount(count + Number(inputValue));
+  };
+
+  const handleDec = () => {
+    setCount(count - Number(inputValue));
+  };
   return (
     <div>
-      <h1>
-        Counter <span>0</span>
+      <h1 data-testid="heading">
+        Counter <span data-testid="count">{count}</span>
       </h1>
-      <button>+</button>
-      <input />
-      <button>-</button>
+      <button data-testid="inc-btn" onClick={handleInc}>
+        +
+      </button>
+      <input
+        placeholder="enter input"
+        data-testid="input"
+        onChange={handleChange}
+        type="number"
+      />
+      <button data-testid="dec-btn" onClick={handleDec}>
+        -
+      </button>
     </div>
   );
 };
--- Jan30StepUp/myreactapphooks/src/Counter/__tests__/Counter.test.js
+++ Jan30StepUp/myreactapphooks/src/Counter/__tests__/Counter.test.js
@@ -1,1 +1,29 @@
-test("", () => {});
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Counter } from "../Counter";
+
+describe("Counter Component", () => {
+  it("should contain +, _,heading and input field", () => {
+    render(<Counter />);
+    const counterText = screen.getByText("Counter");
+    const counterValue = screen.getByTestId("count");
+    const incBtn = screen.getByTestId("inc-btn");
+    const decBtn = screen.getByTestId("dec-btn");
+    const inputField = screen.getByPlaceholderText("enter input");
+    expect(incBtn).toBeInTheDocument();
+    expect(decBtn).toBeInTheDocument();
+    expect(inputField).toBeInTheDocument();
+    expect(counterText).toBeTruthy();
+    expect(counterValue).toBeTruthy();
+  });
+  it("should show entered input data", () => {
+    render(<Counter />);
+    const inputField = screen.getByPlaceholderText("enter input");
+    fireEvent.change(inputField, {
+      target: {
+        value: 123,
+      },
+    });
+    expect(inputField.value).toBe("123");
+    expect(inputField).toHaveDisplayValue("123");
+  });
+});
