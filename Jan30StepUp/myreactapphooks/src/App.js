60839fd8d9aabceda27bcc54016276d7a118c666
ArpithaPuttegowda <arpithalekhya111@gmail.com>
2023-06-12 10:36:16 +0530

    flex

--- Jan30StepUp/myreactapphooks/src/App.js
+++ Jan30StepUp/myreactapphooks/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
-import { CssFlex1 } from "./CSSFlexAndGrid/CssFlex1";
+// import { CssFlex3 } from "./CSSFlexAndGrid/CssFlex3";
+// import { CssFlex1 } from "./CSSFlexAndGrid/CssFlex1";
+import { CssFlex2 } from "./CSSFlexAndGrid/CssFlex2";
 const App = () => {
   return (
     // <div className="container">
@@ -7,7 +9,9 @@ const App = () => {
     //   <div className="item">Item 2</div>
     //   <div className="item">Item 3</div>
     // </div>
-    <CssFlex1 />
+    // <CssFlex1 />
+    <CssFlex2 />
+    // <CssFlex3 />
   );
 };
 
---
+++ Jan30StepUp/myreactapphooks/src/CSSFlexAndGrid/CssFlex3.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CssFlex3.css";
+
+export const CssFlex3 = () => {
+  return (
+    <div class="container">
+      <div class="item">Item 1</div>
+      <div class="item">Item 2</div>
+      <div class="item">Item 3</div>
+      <div class="item">Item 4</div>
+      <div class="item">Item 5</div>
+      <div class="item">Item 6</div>
+    </div>
+  );
+};
