8802c6df78662cf2b55b5ec07a980ec6433a89e0
ArpithaPuttegowda <arpithalekhya111@gmail.com>
2023-07-05 08:27:52 +0530

    c to p

--- StepUp1May/myreactapp/src/Components/ChildToParent/PassingDataFromChildToParent.js
+++ StepUp1May/myreactapp/src/Components/ChildToParent/PassingDataFromChildToParent.js
@@ -1,12 +1,26 @@
 export const Parent3 = () => {
+  const callback = (d) => {
+    alert(d);
+  };
+
   return (
     <div>
-      <h1>I'm Parent</h1>
-      <Child />
+      <h1>I'm Parent....</h1>
+      <Child myData={callback} />
     </div>
   );
 };
 
-const Child = () => {
-  return <div>Child</div>;
+const Child = ({ myData }) => {
+  const data = "Sachin";
+  const handleClick = () => {
+    myData(data);
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>
+        Click me to pass data from child to parent
+      </button>
+    </div>
+  );
 };
