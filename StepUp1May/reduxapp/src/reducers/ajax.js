588e4ef6893160e7ee1a11307fcd9bd6a254d703
ArpithaPuttegowda <arpithalekhya111@gmail.com>
2023-10-04 08:46:14 +0530

    redux-saga

--- StepUp1May/reduxapp/src/reducers/ajax.js
+++ StepUp1May/reduxapp/src/reducers/ajax.js
@@ -1,10 +1,14 @@
 const initData={
-    posts:[]
+    posts:[],
+    my_comments:[]
 }
 
 export const ajaxRed=(state=initData,action)=>{
     if(action.type==="POSTS"){
     state={...state,posts:action.payload}
+    }
+    if(action.type==="MY_COMMENTS"){
+        state={...state,my_comments:action.payload}
     }
     return state
 }
--- StepUp1May/reduxapp/src/saga/ajaxSaga.js
+++ StepUp1May/reduxapp/src/saga/ajaxSaga.js
@@ -1,7 +1,13 @@
-import {takeLatest} from "redux-saga/effects"
+import {call, put, takeLatest} from "redux-saga/effects"
+import {ServerCallService} from  "../ServerCallService/ServerCallService"
 
 function* comments(){
-   alert("comments")
+ const res=yield call(ServerCallService.fetchData,"https://jsonplaceholder.typicode.com/comments")
+ console.log(res,"my comments")
+ yield put({
+    type:"MY_COMMENTS",
+    payload:res.data
+ })
 }
 
 function* post(){
@@ -11,6 +17,7 @@ function* post(){
 function* users(){
     
 }
+
 
 
 function* rootAjax(){
