f4d5a07e2c96968b300f2df85c75e636929c18c5
ArpithaPuttegowda <arpithalekhya111@gmail.com>
2023-10-18 09:20:19 +0530

    flex

--- StepUp1May/myhooks/src/Flex/ExampleFlex.jsx
+++ StepUp1May/myhooks/src/Flex/ExampleFlex.jsx
@@ -2,15 +2,15 @@ import "./ExampleFlex.css"
 export const ExampleFlex = () => {
   return (
     <div id="container">
-        <div id="box-1">BOX1</div>
-        <div id="box-2">BOX2</div>
-        <div id="box-3">BOX3</div>
-        <div id="box-4">BOX4</div>
-        <div id="box-5">BOX5</div>
-        <div id="box-6">BOX6</div>
-        <div id="box-7">BOX7</div>
-        <div id="box-8">BOX8</div>
-        <div id="box-9">BOX9</div>
+        <div id="box-1" className="item">BOX1</div>
+        <div id="box-2" className="item">BOX2</div>
+        <div id="box-3" className="item">BOX3</div>
+        <div id="box-4" className="item">BOX4</div>
+        <div id="box-5" className="item">BOX5</div>
+        <div id="box-6" className="item">BOX6</div>
+        <div id="box-7" className="item">BOX7</div>
+        <div id="box-8" className="item">BOX8</div>
+        <div id="box-9" className="item">BOX9</div>
     </div>
   )
 }
