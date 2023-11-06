e7733bb5073c2766f774919a3f46562152d66a62
ArpithaPuttegowda <arpithalekhya111@gmail.com>
2023-11-06 12:20:52 +0530

    css3 features

--- StepUp1May/myappunittests/src/App.js
+++ StepUp1May/myappunittests/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { ExampleGrid } from './Components/GridSystem/GridLayout'
+import { GridSystem } from './Components/GridSystem/GridSystem'
 
  const App = () => {
   return (
-    <div><ExampleGrid/></div>
+    <div>
+      {/* <ExampleGrid/> */}
+    <GridSystem/>
+    </div>
   )
 }
 export default App
---
+++ StepUp1May/myappunittests/src/Components/GridSystem/GridSystem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./GridSystem.css"
+
+export const GridSystem = () => {
+  return (
+    <div class="grid-container">
+  <div class="grid-item item1">Item 1</div>
+  <div class="grid-item">Item 2</div>
+  <div class="grid-item">Item 3</div>
+</div>
+  )
+}
+ 
